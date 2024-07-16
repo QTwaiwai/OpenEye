d1755c66751ef4d1538c7e7dd1ace051d1e9e7ad
E-n7777 <1301731619@qq.com>
2024-07-16 10:39:40 +0800

    init found_ui

--- module_found/build.gradle.kts
+++ module_found/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 }
 
 android {
+    val isBuildModule = false
     namespace = "com.example.module.found"
     compileSdk = 34
 
@@ -33,13 +34,17 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    viewBinding {
+        enable = true
+    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- module_found/src/main/java/com/example/module/found/FoundFragment.kt
+++
@@ -1,4 +0,0 @@
-package com.example.module.found
-
-class FoundFragment {
-}
