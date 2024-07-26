34e97a92535e9bb8e3cb64b7b6ba352e1a0d4abd
QTwaiwai <2169865863@qq.com>
2024-07-26 21:07:29 +0800

    :lock: :sparkles:

--- module_home/src/main/java/com/example/module/home/ui/activity/HomeActivity.kt
+++ module_home/src/main/java/com/example/module/home/ui/activity/HomeActivity.kt
@@ -1,10 +1,10 @@
-package com.example.module.home
+package com.example.module.home.ui.activity
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import com.example.module.home.databinding.ActivityHomeBinding
 import com.example.module.home.ui.fragment.HomeFragment
 import com.alibaba.android.arouter.facade.annotation.Route
@@ -12,6 +12,8 @@ import com.alibaba.android.arouter.launcher.ARouter
 import com.example.model_hot.ui.HotFragment
 import com.example.module.community.CommunityFragment
 import com.example.module.found.ui.fragment.FoundFragment
+import com.example.module.home.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 /**
  * @author: QT
@@ -21,13 +23,12 @@ import com.example.module.found.ui.fragment.FoundFragment
 
 @Route(path = "/home/HomeActivity")
 class HomeActivity : AppCompatActivity() {
-    private val mBinding: ActivityHomeBinding by lazy {
-        ActivityHomeBinding.inflate(layoutInflater)
-    }
-
+    private var _binding: ActivityHomeBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(mBinding.root)
+        _binding=ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         window.statusBarColor = resources.getColor(R.color.black, null)
         ARouter.getInstance().inject(this)
@@ -40,36 +41,61 @@ class HomeActivity : AppCompatActivity() {
         val foundFragment = FoundFragment()
         val homeFragment = HomeFragment()
         val hotFragment = HotFragment()
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fg_home_view, homeFragment)
-            .commit()
-        mBinding.btnHomeNavigation.setOnItemSelectedListener {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fg_home_view, homeFragment).commit()
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.fg_home_view, foundFragment)
+            add(R.id.fg_home_view, communityFragment)
+            add(R.id.fg_home_view, hotFragment)
+            hide(foundFragment)
+            hide(communityFragment)
+            hide(hotFragment)
+        }.commit()
+        binding.btnHomeNavigation.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.item_home -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fg_home_view, homeFragment)
-                        .commit()
+                    supportFragmentManager.beginTransaction().apply {
+                        hide(foundFragment)
+                        hide(communityFragment)
+                        hide(hotFragment)
+                        show(homeFragment)
+                    }.commit()
                 }
-
                 R.id.item_found -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fg_home_view, foundFragment)
-                        .commit()
+                    supportFragmentManager.beginTransaction().apply {
+                        hide(homeFragment)
+                        hide(communityFragment)
+                        hide(hotFragment)
+                        show(foundFragment)
+                    }.commit()
                 }
 
                 R.id.item_community -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fg_home_view, communityFragment)
-                        .commit()
+                    supportFragmentManager.beginTransaction().apply {
+                        hide(foundFragment)
+                        hide(homeFragment)
+                        hide(hotFragment)
+                        show(communityFragment)
+                    }.commit()
                 }
 
                 R.id.item_mine -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fg_home_view, hotFragment)
-                        .commit()
+                    supportFragmentManager.beginTransaction().apply {
+                        hide(foundFragment)
+                        hide(communityFragment)
+                        hide(homeFragment)
+                        show(hotFragment)
+                    }.commit()
                 }
             }
             true
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.btnHomeNavigation.setOnItemSelectedListener(null)
+        _binding=null
     }
 }
